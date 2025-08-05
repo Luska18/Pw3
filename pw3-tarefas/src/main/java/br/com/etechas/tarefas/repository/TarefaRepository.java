cd82f778da713f5ad2f4569d19f1c768781383e2
Luska18 <lucasdaido07@gmail.com>
2025-08-05 11:01:21 -0300

    a

---
+++ pw3-tarefas/src/main/java/br/com/etechas/tarefas/repository/TarefaRepository.java
@@ -0,0 +1,14 @@
+package br.com.etechas.tarefas.repository;
+
+import br.com.etechas.tarefas.entity.Tarefa;
+
+import java.util.List;
+
+public class TarefaRepository {
+    public void save(Tarefa tarefa) {
+    }
+
+    public List<Tarefa> findAll() {
+        return List.of();
+    }
+}
